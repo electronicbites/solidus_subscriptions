46e15d7b306af99474d687e6f4438a5cb38d544f
Brendan Deere <brendan.g.deere@gmail.com>
2016-12-23 07:46:33 -0800

    Subscriptions have their own shipping methods
    In order to allow for users to manage subscriptions for other people
    (family members etc) we are going to store a shipping address directly
    on the subscription. This way a user can ship different subscriptions to
    different addresses

--- spec/models/solidus_subscriptions/subscription_spec.rb
+++ spec/models/solidus_subscriptions/subscription_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe SolidusSubscriptions::Subscription, type: :model do
   it { is_expected.to have_many :installments }
   it { is_expected.to belong_to :user }
   it { is_expected.to have_one :line_item }
+  it { is_expected.to belong_to :shipping_address }
 
   it { is_expected.to validate_presence_of :user }
   it { is_expected.to validate_presence_of :skip_count }
